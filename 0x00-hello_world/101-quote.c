b41bf80340c43b11966b2d601796d059641de1db
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 22:43:30 +0000

    Advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Accept empty value
+ * strcpy - copies the string pointed to
+ * sizeof - Show the size
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going to print
+ * Return: 1 Value if it works
+ */
+int main(void)
+{
+char string[58];
+strcpy(string, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+puts(string);
+return (1);
+}
