f82210f66a6b8dd0857dc2fa0f869d6214ffa5f1
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 22:05:35 +0000

    Mandatory task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * printf - Prints inputs
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going to print
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+return (0);
+}
