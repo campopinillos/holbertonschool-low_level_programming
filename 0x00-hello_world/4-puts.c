a82af04a93254f7d6d4d43c9e87ffac8a0507ccb
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 21:08:29 +0000

    Mandatory task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Accept empty value
- * string - string value
- * strcpy - copies the string pointed to
  * puts - writes a string to stdout up to but not including the null character
  * @void: Description of a empty parameter
  *
  * Description: This is going to print
- * retun: 0 Value if it works
+ * Return: 0 Value if it works
 */
 int main(void)
 {
-char string[52];
-strcpy(string, "\"Programming is like building a multilingual puzzle");
-puts(string);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
