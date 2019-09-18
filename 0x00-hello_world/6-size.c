6f2702dc7a11e2bc02aac58c941f293ab1a0c798
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 22:18:48 +0000

    Mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,13 @@
  * printf - Prints inputs
  * sizeof - Show the size
  * @void: Description of a empty parameter
- * @int: Integer
- * @char: Character
- * @long int: Long Integer
- * @float: Float
  *
  * Description: This is going to print
  * Return: 0 Value if it works
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n",(int)sizeof(char));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
