0f2101f8e04a97dc1837d66da5ae0a2a735c1026
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 23:42:03 +0000

    Advance task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h> 
 /**
  * main - Accept empty value
- * write - 
+ * write - Screen information
  * @void: Description of a empty parameter
  *
  * Description: This is going to print
@@ -10,6 +10,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
