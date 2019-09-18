907cec44543eb5451b5324b2433482e947b7c9d4
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-18 23:35:31 +0000

    Advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <iostream>
+#include <unistd.h> 
 /**
  * main - Accept empty value
- * cout - is a data stream which is attached to screen output and is used to print to the screen
+ * write - 
  * @void: Description of a empty parameter
  *
  * Description: This is going to print
  * Return: 1 Value if it works
  */
-void main()
+int main(void)
 {
-cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
 return (1);
 }
