63324cb4e43545f9a9d68bdc4c1c01987e392377
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-19 22:14:23 +0000

    Mandatory task

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This sort number from 9 to 10
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 100; x++)
+{
+putchar((x / 10) + '0');
+putchar((x % 10) + '0');
+if (x < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
