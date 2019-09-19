f5c58c09d0c3e5617520b622ed20b1e08a169de0
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-19 23:27:37 +0000

    Mandatory task

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,15 +9,18 @@
  */
 int main(void)
 {
-int x;
-for (x = 0; x < 100; x++)
+int x, y;
+for (x = '0'; x <= '9'; x++)
 {
-putchar((x / 10) + '0');
-putchar((x % 10) + '0');
-if (x < 99)
+for (y = '0'; y <= '9'; y++)
+{
+putchar(x);
+putchar(y);
+if (!(x == '9' && y == '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
