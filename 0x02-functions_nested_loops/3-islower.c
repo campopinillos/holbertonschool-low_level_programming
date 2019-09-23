e7163a6c29ee37019b4f304eac87c2dd1e072ce0
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-23 19:01:17 +0000

    Mandatory task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Accept empty value
+ * _putchar - Print characters one by one
+ * @void: Description of a empty parameter
+ *
+ * Description: This prints the alphabet
+ * Return: 0 Value if it works
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
