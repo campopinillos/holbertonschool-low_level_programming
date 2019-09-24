3d5a7bece6252890a8e071773686ea565d1b238b
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-24 15:40:16 +0000

    Mandatory task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,24 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n < 98)
 	{
-		printf("%d, ", n);
-		n++;
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	printf("98\n");
 	}
-	while (n > 98)
+	else if (n > 98)
 	{
-		printf("%d ,", n);
-		n--;
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	printf("98\n");
 	}
-	printf("%d\n", n);
+	else
+	{
+		printf("98\n");
+	}
 }
