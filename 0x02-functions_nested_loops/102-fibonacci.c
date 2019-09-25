71fcec144f5ed68a58bc1457d3db2ff4014f962d
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-25 02:19:42 +0000

    Advanced task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int x = 0, y = 1, z, w;
+	long int x = 0, y = 1, z, w;
 
 	for (w = 0; w < 50; w++)
 	{
@@ -16,9 +16,9 @@ int main(void)
 		x = y;
 		y = z;
 		if (w < 49)
-			printf("%d, ", z);
+			printf("%li, ", z);
 		else
-			printf("%d\n", z);
+			printf("%li\n", z);
 	}
 	return (0);
 }
