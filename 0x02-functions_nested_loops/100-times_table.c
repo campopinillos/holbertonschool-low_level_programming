f121f51b216df29715a023213ce80fd6a3fccb25
Campo Pinillos <campo.pinillos@gmail.com>
2019-09-25 00:48:03 +0000

    Advance task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,51 @@
-#include <holberton.h>
+#include "holberton.h"
+/**
+ * print_times_table - Prints the times table, starting with 0
+ * @n: The evaluated input
+ *
+ * Description: Prints the times table, starting with 0
+ * Return: 0 Value if it works
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 && n < 15)
+	{
+		for (int x = 0; x <= n; x++)
+		{
+			for (int y = 0; y <= n; y++)
+			{
+				int mult = x * y;
+
+				if (y == 0)
+					_putchar('0');
+				if (y != 0 && mult >= 0 && mult < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mult + '0');
+				}
+				if (mult >= 10 && mult < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mult / 10 + '0');
+					_putchar(mult % 10 + '0');
+				}
+				if (mult >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(mult / 100 + '0');
+					_putchar((mult / 10) % 10 + '0');
+					_putchar(mult % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		return;
+}
