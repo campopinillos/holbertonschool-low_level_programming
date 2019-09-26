277de319872d2f9192a17d8aea507c6306262155
campopinillos <campo.pinillos@gmail.com>
2019-09-26 17:56:25 +0000

    Mandatory task fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,11 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x <= 9)
-				_putchar(x + '0');
-			else
+			if (x >9)
 			{
 				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
 			}
+			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
 	}
