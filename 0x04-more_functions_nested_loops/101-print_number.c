adaf9a155f4a4cb5dcae047b124df2e20be0a4ff
campopinillos <campo.pinillos@gmail.com>
2019-09-27 03:08:27 +0000

    Update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - Print an integer
+ * @n: Parameter input
+ *
+ * Description: Print an integer
+ * Return: 0 Value if it works
+ */
+void print_number(int n)
+{
+
+}
