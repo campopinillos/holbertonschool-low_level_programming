e1b9f8b0c55f30a9bf42293fdff8c37ba0dde041
campopinillos <campo.pinillos@gmail.com>
2019-09-27 02:54:50 +0000

    Advanced task update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Fizz-Buzz test
+ * main - Print the largest prime factor of the number
  * @void: Parameter of times
  *
- * Description: Print Fizz-Buzz
+ * Description: Print the largest prime factor of the number
  * Return: 0 Value if it works
  */
 int main(void)
