3d46bd39f87b73a56bc7773d2ba721997ad471b6
Campo <campo.pinillos@gmail.com>
2019-10-01 16:10:39 -0500

    Advanced task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,12 +14,10 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n *= -1;
-		while (n % x != 0)
-		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			x*=10;
-		}
-		_putchar('\n');
 	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
