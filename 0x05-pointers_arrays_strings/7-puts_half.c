9f17617efedfb2e09262b01e6bbf9a7abda5cc3f
campopinillos <campo.pinillos@gmail.com>
2019-10-01 17:05:11 +0000

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * void puts_half(char *str) - Prints half of a string
+ * @str: The evaluated input
+ *
+ * Description: Prints half of a string
+ * Return: n value
+ */
+void  puts_half(char *str)
+{
+	int n = 0, lenght, x;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	lenght = n;
+	for (x = (lenght / 2); x < lenght; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
