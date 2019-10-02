4c93ebb0db2b1124cd219f2ccc7c5ec13a293321
Campo <campo.pinillos@gmail.com>
2019-10-01 19:18:30 -0500

    Advanced task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that generates random valid passwords 
+ * @void: Empty input
+ *
+ * Description: Program that generates random valid passwords
+ * Return: 0 if works
+ */
+int main(void)
+{
+	return (0);
+}
