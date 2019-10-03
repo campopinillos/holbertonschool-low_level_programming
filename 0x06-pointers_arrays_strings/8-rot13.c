49bddb2b9459562082c8441ce577d7f8f78cd0f5
Campo <campo.pinillos@gmail.com>
2019-10-03 18:07:09 -0500

    Mandatory task

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * rot13 - Encodes a string using rot13
+ * @p: First input pointer
+ *
+ * Description: Encodes a string using rot13
+ * Return: char
+ */
+
+char *rot13(char *p)
+{
+	int i = 0, j;
+	char root1[27] = "ABCDEFGHIJKLMabcdefghijklm";
+	char root2[27] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	while (p[i] != '\0')
+	{
+		j = 0;
+		while (j < 26)
+		{
+			if (p[i] == root1[j])
+				p[i] = root2[j];
+			else if (p[i] == root2[j])
+				p[i] = root1[j];
+			j++;
+		}
+		i++;
+	}
+	return (p);
+}
