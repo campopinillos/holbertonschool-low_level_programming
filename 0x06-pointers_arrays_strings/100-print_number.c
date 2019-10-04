b65eb1b8570a4c24bda86c9d079e87057f65dddd
Campo <campo.pinillos@gmail.com>
2019-10-03 19:37:02 -0500

    Advanced task

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_number - Print an integer
+ * @n: Parameter input
+ *
+ * Description: Print an integer
+ * Return: 0 Value if it works
+ */
+void print_number(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
