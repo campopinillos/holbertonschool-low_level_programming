65ee29abdd12430c7db7853293a336bb8b9b1825
Campo <campo.pinillos@gmail.com>
2019-10-05 12:09:04 -0500

    Advanced task fix 3

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -22,16 +22,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	i--, j--;
 	bigger = (i >= j) ? i + 2 : j + 2;
 	r[bigger--] = 0;
-	while (bigger > 0)
+	while (bigger >= 0)
 	{
 		s1 = (i >= 0) ? (n1[i] - '0') : 0;
 		s2 = (j >= 0) ? (n2[j] - '0') : 0;
 		sum = s1 + s2 + resid;
-		result = (sum > 9) ? sum % 10 : sum;
+		result = (sum > 9) ? sum - 10 : sum;
 		resid = (sum > 9) ? 1 : 0;
 		r[bigger] = result + '0';
 		bigger--, i--, j--;
 	}
-	(resid == 1) ? r[bigger] = 1 + '0' : 0;
+	(resid == 1) ? r[bigger] = 1 + '0' : '\0';
 	return (r);
 }
