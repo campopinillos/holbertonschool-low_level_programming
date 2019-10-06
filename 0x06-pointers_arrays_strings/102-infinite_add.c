dc08143d12a13b51a976bfc9612a324af802d88d
Campo <campo.pinillos@gmail.com>
2019-10-05 23:40:55 -0500

    Advanced task fix

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * infinite_add - Adds two numbers
  * @n1: First input pointer
@@ -11,7 +12,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, count = 0, s1, s2, sum, result, resid = 0, rev;
+	int i = 0, j = 0, s1, s2, sum, result, resid = 0, rev;
 
 	while (n1[i] != 0)
 		i++;
@@ -20,23 +21,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (i + 2 > size_r || j + 2 > size_r)
 		return (0);
 	i--, j--;
+	rev = (i > j) ? i + 2 : j + 2;
+	r[rev--] = 0;
 	while (i >= 0 || j >= 0)
 	{
-		s1 = (i >= 0) ? (n1[i] - '0') : 0;
-		s2 = (j >= 0) ? (n2[j] - '0') : 0;
+		s1 = (i >= 0) ? (n1[i--] - '0') : 0;
+		s2 = (j >= 0) ? (n2[j--] - '0') : 0;
 		sum = s1 + s2 + resid;
 		result = (sum > 9) ? sum % 10 : sum;
 		resid = (sum > 9) ? 1 : 0;
-		r[count] = result + '0';
-		count++, i--, j--;
+		r[rev--] = result + '0';
 	}
-	(resid == 1) ? r[count++] = 1 + '0' : '\0';
-	for (i = count; i > 0 && j < i; i--, j++)
-	{
-		rev = r[j];
-		r[j] = r[i];
-		r[i] = rev;
-	}
-	r[count] = 0;
+	(resid == 1) ? r[rev++] = 1 + '0' : '\0';
+	for (rev = 0; rev < size_r; rev++)
+		if (r[rev] > 57 || r[rev] < 48)
+			r[rev] = 0;
+	
 	return (r);
 }
