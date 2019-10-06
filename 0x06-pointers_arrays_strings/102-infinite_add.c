48e581a7aa3187a13e9dd8d4169490dbc829971e
Campo <campo.pinillos@gmail.com>
2019-10-05 23:52:02 -0500

    Advanced task fix

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -12,17 +12,15 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, s1, s2, sum, result, resid = 0, rev;
+	int i = 0, j = 0, count = 0, s1, s2, sum, result, resid = 0, rev;
 
 	while (n1[i] != 0)
 		i++;
 	while (n2[j] != 0)
 		j++;
-	i--, j--;
-	if (i + 2 > size_r || j + 2 > size_r)
+	if (i > size_r - 2 || j > size_r - 2)
 		return (0);
-	rev = (i > j) ? i + 2 : j + 2;
-	r[rev--] = 0;
+	i--, j--;
 	while (i >= 0 || j >= 0)
 	{
 		s1 = (i >= 0) ? (n1[i--] - '0') : 0;
@@ -30,11 +28,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		sum = s1 + s2 + resid;
 		result = (sum > 9) ? sum % 10 : sum;
 		resid = (sum > 9) ? 1 : 0;
-		r[rev--] = result + '0';
+		r[count] = result + '0';
+		count++;
 	}
-	(resid == 1) ? r[rev++] = 1 + '0' : '\0';
-	for (rev = 0; rev < size_r; rev++)
-		if (r[rev] > 57 || r[rev] < 48)
-			r[rev] = 0;
+	(resid == 1) ? r[count++] = 1 + '0' : '\0';
+	for (i = count; i > 0 && j < i; i--, j++)
+	{
+		rev = r[j];
+		r[j] = r[i];
+		r[i] = rev;
+	}
+	r[count] = 0;
 	return (r);
 }
