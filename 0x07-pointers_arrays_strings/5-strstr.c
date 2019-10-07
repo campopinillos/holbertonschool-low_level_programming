f28f17e8e2e18c910896c814def1d56bfa04a5a2
Campo <campo.pinillos@gmail.com>
2019-10-07 18:08:36 -0500

    Mandatory task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strstr - Locates a substring
+ * @haystack: First input pointer origin
+ * @needle: Second input char
+ *
+ * Description: Locates a substring
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*needle != 0)
+	{
+		while (*haystack != 0)
+		{
+			if (*needle == *haystack)
+				return (haystack);
+			haystack++;
+		}
+		needle++;
+	}
+	return (NULL);
+}
