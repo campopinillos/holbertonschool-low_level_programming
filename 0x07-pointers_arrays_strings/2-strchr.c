0ca56a49b7bcb2061320cf30885690a52e2c6cbf
Campo <campo.pinillos@gmail.com>
2019-10-07 15:53:57 -0500

    Mandatory task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _memcpy - Copies memory area
+ * @dest: First input pointer destination
+ * @src: Second input pointer source
+ * @n: Third input n bytes
+ *
+ * Description: Copies memory area
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != 0)
+	{
+		if (*s == c)
+			break;
+		if (!*s++)
+            return (NULL);
+	}
+	return (s);
+}
