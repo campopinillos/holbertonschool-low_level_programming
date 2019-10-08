d78571986b36ce8be8a31526f30d30c816685f9c
Campo <campo.pinillos@gmail.com>
2019-10-07 19:11:34 -0500

    Mandatory task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,16 +10,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-
-	while (*needle != 0)
+	while (*haystack != 0)
 	{
-		while (*haystack != 0 && *needle != 0)
-		{
-			if (*needle == *haystack)
-				return (haystack);
-			haystack++;
-		}
-		needle++;
+		char *h = haystack;
+		char *n = needle;
+
+		while (*haystack == *n && *n != 0 && *haystack != 0)
+			haystack++, n++;
+		if (*n == 0)
+			return (h);
+		haystack = h + 1;
 	}
 	return (NULL);
 }
