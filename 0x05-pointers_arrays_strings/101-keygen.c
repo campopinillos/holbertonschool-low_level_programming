8ddc9c931202ce652a7cb3a976ac96923805d8b9
Campo <campo.pinillos@gmail.com>
2019-10-10 18:34:24 -0500

    [Fix] Advanced task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,11 +13,12 @@ int main(void)
 	int i = 0, random, checksum;
 
 	srand(time(NULL));
-	checksum = 0;
-	while (checksum < 2772)
+	checksum = 2772;
+	while (checksum > 122)
 	{
-		random = (rand() % 100) + 122;
-		checksum += random;
+		random = (rand() % 100);
+		printf("%c", random);
+		checksum -= random;
 		i++;
 	}
 	printf("%c", checksum);
