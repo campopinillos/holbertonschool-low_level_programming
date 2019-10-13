96c7128e6f3128f5700b869bcecd5093e8056d67
Campo <campo.pinillos@gmail.com>
2019-10-12 22:33:22 -0500

    [Fix] Mandatory task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 /**
  * main - Prints its name
  * @argc: Firts input int argument
- * @argv: Second input char argument of string.
+ * @argv: Second input char argument of string
  *
  * Description: Prints its name followed by a new line
  * Return: 0 if it works
