59cae3e243c4923de463540e64a0a30f4490861a
Campo <campo.pinillos@gmail.com>
2019-10-12 22:22:03 -0500

    [Feature] Mandatory task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all arguments it receives
+ * @argc: Firts input int argument
+ * @argv: Second input char argument of string.
+ *
+ * Description: Prints all arguments it receives
+ * Return: 0 if it works
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+	return (0);
+}
