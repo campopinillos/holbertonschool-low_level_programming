50638760dc672c14ea7709e895b6377c3602a845
Campo <campo.pinillos@gmail.com>
2019-10-12 22:10:09 -0500

    [Feature] Mandatory task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Firts input int argument
+ * @argv: Second input char argument of string.
+ *
+ * Description: Prints the number of arguments passed into it
+ * Return: 0 if it works
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
