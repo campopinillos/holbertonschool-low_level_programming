3a0f6f54ad834f2e0076516cb3ee4a3bc3a2408f
Campo <campo.pinillos@gmail.com>
2019-10-13 00:16:13 -0500

    [Fix] Advanced task improvement

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,19 +13,19 @@ int main(int argc, char *argv[])
 	int i = 0, change = 0, money, resid;
 	int coins[5] = {25, 10, 5, 2, 1};
 
+	money = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	money = atoi(argv[1]);
-	if (money <= 0)
-	{
-		printf("0\n");
-		return (1);
-	}
 	if (argc == 2)
 	{
+		if (money <= 0)
+		{
+			printf("0\n");
+			return (1);
+		}
 		while (i < 5 && money != 0)
 		{
 			resid = money / coins[i];
