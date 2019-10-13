0f99fe0ef4e2ffb857fb92d3ac263a04af0a818e
Campo <campo.pinillos@gmail.com>
2019-10-12 22:04:18 -0500

    [Feature] Mandatory task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints its name
+ * @argc: Firts input int argument
+ * @argv: Second input char argument of string.
+ *
+ * Description: Prints its name followed by a new line
+ * Return: 0 if it works
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
