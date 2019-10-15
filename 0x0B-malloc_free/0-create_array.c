4b942d8301bedef57b27dd806e5c9ef2b28a2cff
Campo <campo.pinillos@gmail.com>
2019-10-15 18:06:29 -0500

    [Feature] Mandatory task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Prints its name
- * @argc: Firts input int argument
- * @argv: Second input char argument of string
+ * create_array - Creates an array of chars, initializes with specific char
+ * @size: Firts input int argument
+ * @c: Second input char argument of string
  *
- * Description: Prints its name followed by a new line
+ * Description: Creates an array of chars
  * Return: 0 if it works
  */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i = 0;
+	unsigned int j = size;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(size * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
+
+	while (j > 0)
+	{
+		str[i] = c;
+		i++, j--;		
+	}
+
+	return (str);
+}
