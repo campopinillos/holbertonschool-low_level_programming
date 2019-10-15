f5a3595a501ec992024fdc7c733f03aca154a9bc
Campo <campo.pinillos@gmail.com>
2019-10-15 17:37:16 -0500

    [fix] Upfdate .gitignore

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * main - Prints its name
+ * @argc: Firts input int argument
+ * @argv: Second input char argument of string
+ *
+ * Description: Prints its name followed by a new line
+ * Return: 0 if it works
+ */
