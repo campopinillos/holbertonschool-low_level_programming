e2e95305f94801d68e41cc60fc84be356dabf8a6
Campo <campo.pinillos@gmail.com>
2019-10-15 23:29:14 -0500

    [Feature] Advance task

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,18 @@
+/**
+ * strtow - Splits a string into words
+ * @str: Firts input string
+ *
+ * Description: Splits a string into words
+ * Return: 0 if it works
+ */
+char **strtow(char *str)
+{
+	char *string;
+	int i = 0, lenght = 1;
+
+	if (str == "" || str == NULL)
+		return (NULL);
+	if (string == NULL)
+		return (NULL);
+	return (string);
+}
