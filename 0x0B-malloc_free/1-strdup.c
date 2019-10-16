b6f44cc9b2111c5dbd65726d77220d0536d86cd0
Campo <campo.pinillos@gmail.com>
2019-10-15 20:42:22 -0500

    [Fix] Mandatory task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,44 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strlen - Returns the length of a string
+ * @s: The evaluated input
+ *
+ * Description: Returns the length of a string
+ * Return: n value
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != 0)
+		n++;
+	return (n);
+}
+
+/**
+ * _strcpy - Copies the string pointed to by src
+ * @dest: Destination
+ * @src: Source
+ *
+ * Description: Copies the string pointed to by src
+ * Return: n value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
 /**
  * _strdup - Returns a pointer to a newly allocated
  * @str: Firts input int argument
@@ -17,9 +54,9 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	string = malloc(strlen(str) + 1);
+	string = malloc(_strlen(str) + 1);
 	if (string == NULL)
 		return (NULL);
-	strcpy(string, str);
+	_strcpy(string, str);
 	return (string);
 }
