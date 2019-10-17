bc6d81bae40cdc97e40a02e396e19eb64c703a1a
Campo <campo.pinillos@gmail.com>
2019-10-17 16:54:46 -0500

    [Feature] Mandatory task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - Creates an array of integers
+ * @min: Firts input int
+ * @max: Second input int
+ *
+ * Description: Creates an array of integers
+ * Return: String
+ */
+int *array_range(int min, int max)
+{
+	int *str;
+	int i = 0, num = 0, size;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	str = malloc(sizeof(max) * size);
+	if (str == 0)
+		return (NULL);
+	while (i <= size)
+		str[i++] = num++;
+	return (str);
+}
