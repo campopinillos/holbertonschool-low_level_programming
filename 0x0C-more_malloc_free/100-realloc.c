ce7a1a88e83fc895404c3c19cb5f5876c06fb3ea
Campo <campo.pinillos@gmail.com>
2019-10-17 18:37:51 -0500

    [Feature] Advanced task 1

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - Reallocates a memory block using malloc and free
+ * @ptr: Firts input pointer
+ * @old_size: Second input unsigned int
+ * @new_size: Third input unsigned int
+ *
+ * Description: Reallocates a memory block
+ * Return: String
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *reserv;
+	unsigned int i = 0;
+	
+	if (old_size == new_size)
+		return (ptr);
+	if (!new_size && ptr)
+	{
+		free (ptr);
+		return (NULL);		
+	}
+	p = (void *) malloc(new_size);
+	if (!p)
+		return (NULL);
+	if (ptr == NULL)
+		return (p);
+	reserv = ptr;
+	while (i < old_size)
+	{
+		p[i] = reserv[i];
+		i++;
+	}
+	free(ptr);
+	return (p);
+}
