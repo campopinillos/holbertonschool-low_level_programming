33c1c8cad4b07f6c043dfc11911a4980bdd8da98
Campo <campo.pinillos@gmail.com>
2019-10-21 12:41:40 -0500

    [Feature] Mandatory task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the file it was compiled from
+ * @void: Empty argument
+ *
+ * Description: Prints the name of the file it was compiled from
+ * Return: 0 if it works
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
