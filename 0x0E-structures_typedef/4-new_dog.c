7ae205050c6c009b8325c21d0095aac10680cffb
Campo <campo.pinillos@gmail.com>
2019-10-21 18:30:21 -0500

    [Fix] Mandatory task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer structure
+ *
+ * Description: Prints a struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
