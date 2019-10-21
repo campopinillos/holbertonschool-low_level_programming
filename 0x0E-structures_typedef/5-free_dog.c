9b57b20efad3897e72c0b68f54c5b87d532ab34e
Campo <campo.pinillos@gmail.com>
2019-10-21 18:26:50 -0500

    [Feature] Mandatory task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees dogs structure
+ * @d:  struct dog to be initialized
+ *
+ * Description: Frees dogs structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
