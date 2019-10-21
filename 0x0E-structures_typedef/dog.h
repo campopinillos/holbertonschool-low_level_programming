64fe878c4a7046710eb14541cb6c28c1e6da1985
Campo <campo.pinillos@gmail.com>
2019-10-21 15:22:33 -0500

    [Fix] Mandatory task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - Short description
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: Longer description
+ */
 
 struct dog
 {
 	char *name;
-    float age;
-    char *owner;
+	float age;
+	char *owner;
 };
 
 #endif /* _DOG_H_ */
