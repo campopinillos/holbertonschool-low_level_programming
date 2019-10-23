e9413e1941bf09d0badaea6f538ab0c2667cdf24
Campo <campo.pinillos@gmail.com>
2019-10-23 15:00:18 -0500

    [Feature] Mandatory task 2 upload

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer
+ * @array: Array
+ * @size: Size of the array
+ * @cmp: Pointer to the function used to compare value
+ *
+ * Description: Executes a function given as a parameter
+ * on each element of an array
+ * Return: Empty
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && cmp)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i++]) == 0)
+				continue;
+			else
+				break;
+		}
+	}
+	return (i - 1);
+}
