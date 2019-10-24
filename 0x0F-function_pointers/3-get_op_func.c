2f0d4a6aa52e6af74180a57c39f1b9b37d978687
Campo <campo.pinillos@gmail.com>
2019-10-23 20:20:24 -0500

    [Fix] Mandatory task 3 exit update and get_op fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0] && !s[1])
+		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,13 +26,13 @@ int main(int argc, char *argv[])
 	if (num2 == 0 && (operator[0] == '/' || operator[0] == '%'))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 	op_func = get_op_func(operator);
 	if (!op_func)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	calc = op_func(num1, num2);
 	printf("%d\n", calc);
