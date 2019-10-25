d12a389fac3af473a5cd0dee6181641b40944684
Campo <1207@holbertonschool.com>
2019-10-25 17:40:41 -0500

    [Fix] Betty requirements

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
  * print_numbers - Prints numbers
  * @separator:  String to be printed between numbers
  * @n: Number of integers passed to the function
- * 
+ *
  * Description: Prints numbers,
  * followed by a new line
  * sum of all its parameters.
@@ -14,15 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list arguments;
-	
+
 	va_start(arguments, n);
 	while (i < n)
 	{
-		printf("%d",va_arg(arguments, int));
+		printf("%d", va_arg(arguments, int));
 		if (separator && n > i + 1)
 			printf("%s", separator);
 		i++;
 	}
 	va_end(arguments);
-	printf("\n");	
-
+	printf("\n");
+}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
  * print_strings - Prints strings
  * @separator:  String to be printed between strings
  * @n: Number of integers passed to the function
- * 
+ *
  * Description: Prints strings,
  * followed by a new line
  * sum of all its parameters.
@@ -15,13 +15,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *string;
 	va_list arguments;
-	
+
 	va_start(arguments, n);
 	while (i < n)
 	{
 		string = va_arg(arguments, char *);
 		if (string)
-			printf("%s",string);
+			printf("%s", string);
 		if (!string)
 			printf("(nil)");
 		if (separator && n > i + 1)
@@ -29,5 +29,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		i++;
 	}
 	va_end(arguments);
-	printf("\n");	
+	printf("\n");
 }
