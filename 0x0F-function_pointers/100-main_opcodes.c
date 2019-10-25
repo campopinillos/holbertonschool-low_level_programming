fa37440f39d15f597057bf1d11c0600280e595d1
Campo <1207@holbertonschool.com>
2019-10-24 19:36:22 -0500

    [Feature] Advanced task upload

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - Prints the opcodes
+ * @argc: Amount of inputs
+ * @argv: Char pointer of arguments
+ *
+ * Description: Prints the opcodes of
+ * its own main function
+ * Return: O if it works
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, bytes;
+
+	if (argc != 2)
+	{
+		puts("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		puts("Error\n");
+		exit(2);
+	}
+	while (i < bytes)
+	{
+		printf("%02hhx", *((char *) (main + i)));
+		if (bytes > i + 1)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
