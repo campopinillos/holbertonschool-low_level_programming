5f422ec50401c0ccc6996c5db5489f1c23fea87f
Campo Pinillos <campopinillos@MacBook-Pro-de-Campo.local>
2019-10-27 13:58:19 -0500

    [Fix] Repare variable declaration

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,31 +10,31 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *string, *c = "";
+	char *string, *com = "";
 	va_list arguments;
 
 	va_start(arguments, format);
 	i = 0;
-	while (format && format[i])
+	while (format[i])
 	{
 		if (format[i + 1])
 			c = ", ";
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c%s", (char) va_arg(arguments, int), c);
+			printf("%c%s", (char) va_arg(arguments, int), com);
 			break;
 		case 'i':
-			printf("%d%s", va_arg(arguments, int), c);
+			printf("%d%s", va_arg(arguments, int), com);
 			break;
 		case 'f':
-			printf("%f%s", va_arg(arguments, double), c);
+			printf("%f%s", va_arg(arguments, double), com);
 			break;
 		case 's':
 			string = va_arg(arguments, char *);
 			if (!string)
 				string = "(nil)";
-			printf("%s%s", string, c);
+			printf("%s%s", string, com);
 			break;
 		}
 		i++;
