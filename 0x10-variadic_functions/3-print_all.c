ccebc0fb7316beb90342baf7bed4a434ecbdd155
Campo Pinillos <campopinillos@MacBook-Pro-de-Campo.local>
2019-10-27 13:43:22 -0500

    [Fix] Update conditional for commas

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,35 +9,35 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	char *string;
+	int i = 0;
+	char *string, *c = "";
 	va_list arguments;
 
 	va_start(arguments, format);
 	i = 0;
 	while (format[i] != 0)
 	{
+		if (i >= 0 && format[i + 1] != 0)
+			c = ", ";
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", (char) va_arg(arguments, int));
+			printf("%c%s", (char) va_arg(arguments, int), c);
 			break;
 		case 'i':
-			printf("%d", va_arg(arguments, int));
+			printf("%d%s", va_arg(arguments, int), c);
 			break;
 		case 'f':
-			printf("%f", va_arg(arguments, double));
+			printf("%f%s", va_arg(arguments, double), c);
 			break;
 		case 's':
 			string = va_arg(arguments, char *);
 			if (string)
-				printf("%s", string);
+				printf("%s%s", string, c);
 			else
-				printf("%p", string);
+				printf("%p%s", string, c);
 			break;
 		}
-		if (i > 0 && format[i + 1] != 0)
-			printf(", ");
 		i++;
 	}
 	va_end(arguments);
