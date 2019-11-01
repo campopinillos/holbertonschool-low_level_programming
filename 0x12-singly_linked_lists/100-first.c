601cf9f282c422cacda1d208822f4e6e05e54dc1
campopinillos <1207@holbertonschool.com>
2019-10-31 23:02:28 -0500

    [Feature] Advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+    return (0);
+}
