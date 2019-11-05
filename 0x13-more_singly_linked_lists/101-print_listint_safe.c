80699ebf26b2ecf3af6abddf50eb3001aeab8cb9
campopinillos <1207@holbertonschool.com>
2019-11-05 16:52:02 -0500

    [Feature] Advanced task 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,5 +8,18 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t count = 0;
+	const listint_t *tmp;
 
+	tmp = head;
+	while (tmp)
+	{
+		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		tmp = tmp->next;
+		count++;
+		
+		if (!head)
+			exit(98);
+	}
+	return (count);
 }
