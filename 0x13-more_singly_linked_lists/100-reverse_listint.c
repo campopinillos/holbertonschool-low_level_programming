e944590e5ff5207dac2cfa9f640115a9f94215fb
campopinillos <1207@holbertonschool.com>
2019-11-05 14:27:41 -0500

    [Feature] Advanced task try 1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,5 +8,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *head1, *tail;
 
+	if (!head && !*head)
+		return (NULL);
+	head1 = *head;
+	*head = NULL;
+	while (head1)
+	{
+		tail = head1;
+		head1 = head1->next;
+		tail->next = *head;
+		*head = tail;
+	}
+	return (*head);
 }
