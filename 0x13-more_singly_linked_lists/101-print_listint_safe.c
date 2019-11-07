d39da8013f14384b616823ee820d061c245de320
campopinillos <1207@holbertonschool.com>
2019-11-06 23:18:47 -0500

    [Fix] File conditional

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,18 +8,29 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *tmp;
+	size_t count = 0, i = 0;
+	const listint_t *tmp, *tmp1;
 
+	if (!head)
+		exit(98);
 	tmp = head;
 	while (tmp)
 	{
+		tmp1 = head;
 		printf("[%p] %d\n", (void *)tmp, tmp->n);
-		tmp = tmp->next;
+		tmp = (*tmp).next;
 		count++;
-		
-		if (!head)
-			exit(98);
+		i = 0;
+		while (i < count)
+		{
+			if (tmp == tmp1)
+			{
+				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+				return (count);
+			}
+			tmp1 = (*tmp1).next;
+			i++;
+		}
 	}
 	return (count);
 }
