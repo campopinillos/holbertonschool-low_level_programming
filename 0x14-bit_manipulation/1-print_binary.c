5cf417f0f0837926d01df43c42471b39962f36a3
campopinillos <1207@holbertonschool.com>
2019-11-08 14:55:22 -0500

    [Feature] Mandatory task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: Number
+ *
+ * Description: Prints the binary representation of a number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int bin, count;
+	int c = sizeof(n) * 8;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (c >= 0)
+	{
+		bin = n >> (c - 1);
+		if (bin & 1)
+			count = 1;
+		if (count == 1)
+			_putchar((bin & 1) + '0');
+		 c--;
+	}
+}
