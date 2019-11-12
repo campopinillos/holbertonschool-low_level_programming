98df1b49cb8f3ae5849efd1f44d7581533435537
campopinillos <1207@holbertonschool.com>
2019-11-12 17:22:20 -0500

    [Feature] Mandatory task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * create_file - Create a function that creates a file
+ * @filename: file name
+ * @text_content: Content to file
+ *
+ * Description: Create a function that creates a file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, lenght = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	lenght = _strlen(text_content);
+	w = write(fd, text_content, lenght);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
