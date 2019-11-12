1472ef6828bf864221004eefd535c052443ca7f5
campopinillos <1207@holbertonschool.com>
2019-11-12 16:21:05 -0500

    [Fix] Mandatory task malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,18 +10,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer;
 	int fd, r, w;
-	char *buffer[letters];
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
+		return (0);
 	r = read(fd, buffer, letters);
 	if (r == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	w = write(STDOUT_FILENO, buffer, r);
 	if (w == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	close(fd);
+	free(buffer);
 	return (w);
 }
