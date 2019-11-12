7930cc8c6f0f09326384f61117c8e181d5c55607
campopinillos <1207@holbertonschool.com>
2019-11-12 17:47:12 -0500

    [Fix] Mandatory task 2 append flag

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,44 @@
 #include "holberton.h"
 int _strlen(char *s);
 /**
- * create_file - Create a function that creates a file
+ * create_file - Appends text at the end of a file
  * @filename: file name
- * @text_content: Content to file
+ * @text_content: Content to append to a file
  *
- * Description: Create a function that creates a file
- * Return: 1 on success, -1 on failure (file can not be created,
- * file can not be written, write “fails”
+ * Description: Appends text at the end of a file
+ * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w, lenght = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = "";
+		return (1);
 	lenght = _strlen(text_content);
 	w = write(fd, text_content, lenght);
 	if (w == -1)
 		return (-1);
 	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The evaluated input
+ *
+ * Description: Returns the length of a string
+ * Return: n value
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	return (n);
 }
