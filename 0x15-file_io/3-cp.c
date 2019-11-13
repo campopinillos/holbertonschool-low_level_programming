ff877424d588547f7e291811b2240b847b7bb0c8
campopinillos <1207@holbertonschool.com>
2019-11-12 21:52:39 -0500

    [Fix] Mandatory task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,32 +9,37 @@
  */
 int main(int ac, char **av)
 {
-	int fd, fdw, rd, wr;
+	int fd, fdw;
 	char buffer[SIZE], *file_from, *file_to;
+	ssize_t rd = 1024, wr;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!av[1])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(97);
-	file_from = av[1], file_to = av[2];
+	file_from = av[1];
+	file_to = av[2];
 	fd = open(file_from, O_RDONLY);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fdw = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	fdw = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdw == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
-	rd = read(fd, buffer, SIZE);
-	if (rd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	while (rd > 0)
+	while (rd == SIZE)
 	{
+		rd = read(fd, buffer, SIZE);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
 		wr = write(fdw, buffer, rd);
 		if (wr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	}
-	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	if (close(fdw) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw), exit(100);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	return (0);
 }
