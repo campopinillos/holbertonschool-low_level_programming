c45a25e0d668830d3bc534f72876a97ac0fca6f9
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-16 12:07:59 -0500

    [Feature] Advanced tasks 3

---
+++ 0x18-dynamic_libraries/c_functions/op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * add - Return the sum of a and b
+ * @a: First int input
+ * @b: Second int input
+ *
+ * Description: This function returns the
+ * sum of a and b
+ * Return: The sum of the input parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Returns the difference of a and b
+ * @a: First int input
+ * @b: Second int input
+ *
+ * Description: This function returns the
+ * difference of a and b
+ * Return: The difference of the input parameters
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Returns the product of a and b
+ * @a: First int input
+ * @b: Second int input
+ *
+ * Description: This function returns the
+ * product of a and b
+ * Return: The product of the input parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Returns the division of a by b
+ * @a: First int input
+ * @b: Second int input
+ *
+ * Description: This function returns the result
+ * of the division of a by b
+ * Return: The division of the input parameters
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Returns the remainder of a by b
+ * @a: First int input
+ * @b: Second int input
+ *
+ * Description: This function returns the remainder
+ * of the division of a by b
+ * Return: The remainder of the input parameters
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
