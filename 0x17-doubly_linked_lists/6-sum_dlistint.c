f42b1e7cf187472229014ea146f13850e2e47832
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-15 21:11:49 -0500

    [Feature] Mandatory tasks 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Returns the nth node of a linked list
+ * @head: Doubly linked lists head
+ *
+ * Description: Returns the nth node of a dlistint_t linked list
+ * Return: Index
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
