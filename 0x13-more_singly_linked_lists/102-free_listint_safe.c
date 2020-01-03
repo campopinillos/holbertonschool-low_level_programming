a12ecf2adb1ab11b8fd17e42dfb72695176b0c1a
Campo Elias Pinillos <campo.pinillos@gmail.com>
2020-01-02 20:59:49 -0500

    [Fix] Code Advanced task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,5 +8,21 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *safe_node, *tmp;
+	size_t n = 0;
 
+	if (!h)
+		return (0);
+	while (*h)
+	{
+		safe_node = (*h)->next;
+		tmp = *h;
+		free(*h);
+		*h = NULL;
+		n++;
+		if (tmp < safe_node)
+			break;
+		*h = safe_node;
+	}
+	return (n);
 }
