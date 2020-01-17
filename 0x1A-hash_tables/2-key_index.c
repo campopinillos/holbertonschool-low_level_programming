962c77f4ef41f0da4807cbe32abafe6ca39e4b37
campopinillos <1207@holbertonschool.com>
2020-01-17 18:26:00 -0500

    [Feature] Task files upload

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - Function that implements the djb2 algorithm
+ * write - Screen information
+ * @key: Size of the hash table
+ *
+ * Description: Functionimplements the djb2 algorithm
+ * Return: A hash
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
