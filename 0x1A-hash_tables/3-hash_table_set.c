77fbc108ff363f510379aa0447aeb1ccc20133cb
campopinillos <1207@holbertonschool.com>
2020-01-17 22:57:09 -0500

    [Update] Mandatory task files

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Function that adds an element to the hash table
+ * @ht: Hash table you want to add or update the key/value to
+ * @key: Key of the hash table
+ * @value: Value associated with the key
+ *
+ * Description: Function that adds an element to the hash table
+ * In case of collision, add the new node at the beginning of the list
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	
+}
