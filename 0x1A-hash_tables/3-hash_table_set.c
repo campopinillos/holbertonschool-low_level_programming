354e041923b2bd9d78b00b61be00dad8e24c109e
campopinillos <1207@holbertonschool.com>
2020-01-18 22:05:06 -0500

    [Fix] Update code and header

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,5 +11,38 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	unsigned long int index, i;
+	hash_node_t *node = NULL;
+
+	if (!ht || !key || !*key || !value || !strdup(value))
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; ++i)
+	{
+		if (!strcmp(ht->array[i]->key, key))
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
