1ed38cb69cadb35bd97e3b6494cf062801d3d247
campopinillos <1207@holbertonschool.com>
2020-04-21 12:11:04 -0500

    [Fix] betty style and code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,15 +11,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	int i = 0;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	for (i = 0; i < (int)size; i++)
+	while (i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		i++
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -16,9 +16,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -34,14 +34,14 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 /* Functions */
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
